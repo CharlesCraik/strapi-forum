66fa2d1d25e294a3f52814c6fb55cb3cc31529b9
Charles Craik <c.craik2000@gmail.com>
2023-03-21 08:26:15 +0000

    topic card styling

--- src/components/cards/topic-card.js
+++ src/components/cards/topic-card.js
@@ -9,8 +9,8 @@ const TopicCard = (props) => {
         <div className="topic-card column">
             <div className="topic-card-head row just-sb align-c">
                 <span className="topic-title">{props.title}</span>
-                <div className="topic-short-actions-container row align-c">
-                    <button className="BTN icon" name="save-for-later"><Archive size="24" color="#FF8A65" variant="Outline"/></button>
+                <div className="topic-short-actions-container row align-c just-e">
+                    <button className="BTN icon" name="save-for-later"><Archive size="24" color="#171E45" variant="Outline"/></button>
                 </div>
             </div>
             <div className="topic-card-meta row just-sb align-c">
@@ -19,14 +19,12 @@ const TopicCard = (props) => {
                     <span className="topic-publish-date">{publishDate.toLocaleDateString()}</span>
                 </div>
                 <div className="topic-categories-container row align-c just-e">
-                    <span className="bubble topic-category">Category 1</span>
-                    <span className="bubble topic-category">Category 2</span>
-                    <span className="bubble topic-category">Category 3</span>
+                    
                 </div>
             </div>
             <div className="topic-content-container column align-s">
                 <p>{props.content}</p>
-                <a className="BTN" href={window.location.pathname + '/' + props.slug}>Respond</a>
+                <a className="BTN primary" href={window.location.pathname + '/' + props.slug}>Respond</a>
             </div>
         </div>
     );
