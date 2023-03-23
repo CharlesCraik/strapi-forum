3733f417c6fdb938e832b059a5b79746927b7add
Charles Craik <c.craik2000@gmail.com>
2023-03-23 06:47:39 +0000

    comment card styling

--- src/components/cards/comments-card.js
+++ src/components/cards/comments-card.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 
 const CommentCard = (props) => {
+    const [publishDate] = useState(new Date(props.published));
+
     return(
         <div className="comment-card column">
             <div className="comment-head">
@@ -9,9 +11,12 @@ const CommentCard = (props) => {
             <div className="comment-body">
                 <p>{props.content}</p>
             </div>
-            <div className="comment-footer comment-meta row align-c">
+            <div className="comment-footer comment-meta row align-c just-sb">
                 <div className="author-container">
                     <span className="author">Posted by <b>{props.author}</b></span>
+                </div>
+                <div className="date-container">
+                    <span className="date">{publishDate.toLocaleDateString()}</span>
                 </div>
             </div>
         </div>
